fc6d5dbc2c4901cf9a31c0c5567bd2e7b715401a
Spencer Lin <linspenc@usc.edu>
2025-01-25 11:42:14 -0500

    add avp components to normcore test scene

--- Assets/Bublisher/3D Stylized Animated Dogs Kit/Scripts/DemoController.cs
+++ Assets/Bublisher/3D Stylized Animated Dogs Kit/Scripts/DemoController.cs
@@ -17,14 +17,14 @@ public class DemoController : MonoBehaviour
 
         private void Start()
     {
-            thing.GetComponent<AIClient>().textInput_.onValueChanged.AddListener(delegate { myTextChanged(); });
+            // thing.GetComponent<AIClient>().textInput_.onValueChanged.AddListener(delegate { myTextChanged(); });
 
-            if (thing.GetComponent<AIClient>())
-            {
-              //  if(thing.GetComponent<AIClient>().textInput_.text== "")
-                    if (thing.GetComponent<AIClient>().currState == "")
-                        Debug.Log("yes breath!!!");
-            }
+            // if (thing.GetComponent<AIClient>())
+            // {
+            //   //  if(thing.GetComponent<AIClient>().textInput_.text== "")
+            //         if (thing.GetComponent<AIClient>().currState == "")
+            //             Debug.Log("yes breath!!!");
+            // }
        foreach (var button in _buttons)
        {
           button.Click += OnAnimationButtonClick;
