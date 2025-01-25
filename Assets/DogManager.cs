0735299c048ba4f98ef4fbd55c2605b3904a6b55
Brenda <bpadilladejesus@albany.edu>
2025-01-25 16:42:59 -0500

    dog dropdown test

--- Assets/DogManager.cs
+++ Assets/DogManager.cs
@@ -6,26 +6,29 @@ using System.Collections.Generic;
 public class DogManager : MonoBehaviour
 {
     public GameObject[] dogs;  // Array to hold different dog GameObjects
-    public Dropdown dogDropdown;  // Reference to the UI Dropdown component
+    // public Dropdown dogDropdown;  // Reference to the UI Dropdown component
+
+    [SerializeField]
+    private GameObject currentActiveDog;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (dogs.Length > 0)
-        {
-            // Populate the dropdown with the names of the dog objects
-            dogDropdown.ClearOptions();
-            var dogNames = new List<string>();
-            foreach (var dog in dogs)
-            {
-                dogNames.Add(dog.name);
-            }
-            dogDropdown.AddOptions(dogNames);
-        }
-        else
-        {
-            Debug.LogWarning("No dogs assigned in the array.");
-        }
+        // if (dogs.Length > 0)
+        // {
+        //     // Populate the dropdown with the names of the dog objects
+        //     dogDropdown.ClearOptions();
+        //     var dogNames = new List<string>();
+        //     foreach (var dog in dogs)
+        //     {
+        //         dogNames.Add(dog.name);
+        //     }
+        //     dogDropdown.AddOptions(dogNames);
+        // }
+        // else
+        // {
+        //     Debug.LogWarning("No dogs assigned in the array.");
+        // }
     }
 
     // When the button is clicked, toggle the selected dog
@@ -38,9 +41,10 @@ public class DogManager : MonoBehaviour
         GameObject selectedDog = dogs[selectedIndex];  // Get the dog based on dropdown selection
 
         // Toggle the active state of the selected dog
-        if (selectedDog != null)
+        if (selectedDog != currentActiveDog)
         {
-            selectedDog.SetActive(!selectedDog.activeSelf);
+            currentActiveDog.SetActive(false);
+            selectedDog.SetActive(true);
         }
     }
 }
