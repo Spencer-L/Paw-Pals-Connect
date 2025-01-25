f8213a1f6771ba7095c9497caae69ef5bcea3fb2
ErinMitt <33756931+ErinMitt@users.noreply.github.com>
2025-01-25 08:03:24 -0800

    added text stuff

--- Assets/Bublisher/3D Stylized Animated Dogs Kit/Scripts/DemoController.cs
+++ Assets/Bublisher/3D Stylized Animated Dogs Kit/Scripts/DemoController.cs
@@ -2,6 +2,8 @@ using Mangrove;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace DemoKitStylizedAnimatedDogs 
 {
@@ -11,19 +13,31 @@ public class DemoController : MonoBehaviour
     [SerializeField] private List<AnimationButton> _buttons;
     [SerializeField] private List<Animator> _animators;
         AIClient aiClient;
+      //  public InputField textInput_;
+
         private void Start()
     {
+            thing.GetComponent<AIClient>().textInput_.onValueChanged.AddListener(delegate { myTextChanged(); });
+
             if (thing.GetComponent<AIClient>())
             {
-                if(thing.GetComponent<AIClient>().currState=="")
-                Debug.Log("yes breath!!!");
+              //  if(thing.GetComponent<AIClient>().textInput_.text== "")
+                    if (thing.GetComponent<AIClient>().currState == "")
+                        Debug.Log("yes breath!!!");
             }
        foreach (var button in _buttons)
        {
           button.Click += OnAnimationButtonClick;
        }
-    }
-    public void SwitchAnim()
+            //thing.onValueChanged.AddListener(myTextChanged);
+        }
+        public void myTextChanged()
+        {
+            Debug.Log("wheeeee");
+        }
+            // We need this signature in order to directly add and remove this method as 
+            // a listener for the onValueChanged event
+        public void SwitchAnim()
         {
             _animators[1].SetInteger("AnimationID", 1);
 
