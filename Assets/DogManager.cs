ea73fc759becd4abf2ec1e6e699f813121281238
Brenda <bpadilladejesus@albany.edu>
2025-01-25 13:30:09 -0500

    Update DogManager

--- Assets/DogManager.cs
+++ Assets/DogManager.cs
@@ -1,28 +1,43 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DogManager : MonoBehaviour
 {
-    public GameObject dog;
+    public GameObject[] dogs;  // Array to hold different dog GameObjects
+    public Dropdown dogDropdown;  // Reference to the UI Dropdown component
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called before the first frame update
     void Start()
     {
-        
+        if (dogs.Length > 0)
+        {
+            // Populate the dropdown with the names of the dog objects
+            dogDropdown.ClearOptions();
+            var dogNames = new List<string>();
+            foreach (var dog in dogs)
+            {
+                dogNames.Add(dog.name);
+            }
+            dogDropdown.AddOptions(dogNames);
+        }
+        else
+        {
+            Debug.LogWarning("No dogs assigned in the array.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // When the button is clicked, toggle the selected dog
     public void WhenButtonClicked()
     {
-        if (dog.activeInHierarchy == true)
-            dog.SetActive(false);
-        else
-            dog.SetActive(true);
-        
-    }
+        if (dogs.Length == 0) return;
 
+        int selectedIndex = dogDropdown.value;  // Get the selected dog index from the dropdown
+        GameObject selectedDog = dogs[selectedIndex];  // Get the dog based on dropdown selection
+
+        // Toggle the active state of the selected dog
+        if (selectedDog != null)
+        {
+            selectedDog.SetActive(!selectedDog.activeSelf);
+        }
+    }
 }
